0be55c8f8d18278a3c3bfdd8cd452a64def2a67a
test-amina <102810422+test-amina@users.noreply.github.com>
2022-04-01 22:41:03 +0900

    Create 6-1-2.js

---
+++ 6-1-2.js
@@ -0,0 +1,69 @@
+	$('.slider').slick({
+		fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+		speed:1000,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: false,//左右の矢印あり
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		dots: true,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
+});
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+// img-gallery
+
+$('.slider-img-gallery').slick({
+	arrows: false,//左右の矢印はなし
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+	speed: 6900,//スライドのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+	pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+	cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+	slidesToShow: 4,//スライドを画面に4枚見せる
+	slidesToScroll: 1,//1回のスライドで動かす要素数
+	responsive: [
+		{
+		breakpoint: 769,//モニターの横幅が769px以下の見せ方
+		settings: {
+			slidesToShow: 2,//スライドを画面に2枚見せる
+		}
+	},
+	{
+		breakpoint: 426,//モニターの横幅が426px以下の見せ方
+		settings: {
+			slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+		}
+	}
+]
+});
+
+// スクロール
+$('a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
